587034f8ff57c38afd0c5e1d5d01b99f5fdeaaf7
shruti-perpetualny <shruti@perpetualny.com>
2017-11-22 20:14:32 +0530

    implemented blog functionaity

--- Flask-Pracs/app/__init__.py
+++ Flask-Pracs/app/__init__.py
@@ -4,6 +4,8 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_admin import Admin
 
 from flask_admin.contrib.sqla import ModelView
+from flask_login import LoginManager
+
 
 # Import a module / component using its blueprint handler variable (mod_auth)
 
@@ -19,6 +21,8 @@ app.config.from_object('config')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:root@localhost/testuser'
 app.config['SECRET_KEY'] = 'b240ac48e41e84a4278d195092289a8bdb08556b22f6760d'
 db = SQLAlchemy(app)
+lm = LoginManager(app)
+lm.session_protection ='Strong'
 
 from app.auth import views, models
 from app.auth import auth as auth_blueprint
@@ -30,6 +34,7 @@ app.register_blueprint(auth_blueprint)
 # Build the database:
 # This will create the database file using SQLAlchemy
 admin.add_view(ModelView(models.User, db.session))
+admin.add_view(ModelView(models.Blog, db.session))
 
 from app.auth.models import *
 
